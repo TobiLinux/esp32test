8c485fb7ecb2e1187203853d7f134e2658b51fef
TobiLinux <48185015+TobiLinux@users.noreply.github.com>
2022-12-27 12:57:15 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from machine import Pin, I2C
 
 # using default address 0x3C
 i2c = I2C(sda=Pin(21), scl=Pin(22))
-display = ssd1306.SSD1306_I2C(128, 64, i2c)
+display = ssd1306.SSD1306_I2C(128, 32, i2c)
 
 display.text('Hello, World!', 0, 0, 1)
 display.show()
