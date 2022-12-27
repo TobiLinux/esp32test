172a75fbed3182debec24bf29780e5613107984b
TobiLinux <48185015+TobiLinux@users.noreply.github.com>
2022-12-26 23:34:45 -0300

    Create run.py

---
+++ run.py
@@ -0,0 +1,12 @@
+import esp32, time
+
+hall = esp32.hall_sensor()     # read the internal hall sensor
+temp = esp32.raw_temperature() # read the internal temperature of the MCU, in Fahrenheit
+#esp32.ULP()             # access to the Ultra-Low-Power Co-processor
+
+while True:
+  hall = esp32.hall_sensor()     # read the internal hall sensor
+	temp = esp32.raw_temperature() # read the internal temperature of the MCU, in Fahrenheit
+	print(hall)
+	print(temp)
+	time.sleep(2)
