46ec156e3361665d2a9c7a1e89b0be68d9d9559d
TobiLinux <48185015+TobiLinux@users.noreply.github.com>
2022-12-27 00:07:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import esp32, time
 print('Corriendo main.py')
 while True:
+	print('<-+-+-+-+-+-+-+-+-+-+->'')
 	hall = esp32.hall_sensor()     # read the internal hall sensor
 	temp = esp32.raw_temperature() # read the internal temperature of the MCU, in Fahrenheit
 	print('Hall Sensor: ', hall)
 	print('Temperatura: ', temp)
-	time.sleep(2)
+	time.sleep(1)
